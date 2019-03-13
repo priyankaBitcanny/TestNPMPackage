ab26d97c7587342128d7ea6462bd3698ee5eea7c
priyankaBitcanny <priyanka@bitcanny.com>
2019-03-13 13:06:29 +0530

    test start scan method added

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 
+
+export default class oaksApi {
+
+  constructor(nativeV3Module){
+
+    self.v3Module = nativeV3Module;
+
+  }
+  startScan = () => {
+    console.log('===> npm package startScan called');
+    self.v3Module.startScan();
+  }
+}
+
